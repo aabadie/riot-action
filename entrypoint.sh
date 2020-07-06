0589d3ecca3a3db885c0a60eae9d3bb5cd21f479
Alexandre Abadie <alexandre.abadie@inria.fr>
2020-07-06 15:07:21 +0200

    entrypoint.sh: fix static test run in action

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,4 +2,8 @@
 
 set -e
 
-cd ${GITHUB_WORKSPACE} && bash -c "${INPUT_CMD}"
+git config --system user.name "riot" && \
+git config --system user.email "ci@riot-os.org"
+export CI_BASE_BRANCH=origin/master
+
+cd ${GITHUB_WORKSPACE}&& git fetch origin master && bash -c "${INPUT_CMD}"
