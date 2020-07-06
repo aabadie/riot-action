8236197411ffa8928081a0999840ae7d100902c1
Alexandre Abadie <alexandre.abadie@inria.fr>
2020-07-06 11:48:34 +0200

    entrypoint.sh: fetch RIOT master branch before running the command

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,4 +2,6 @@
 
 set -e
 
-cd ${GITHUB_WORKSPACE} && bash -c "${INPUT_CMD}"
+RIOT_REPO = "https://github.com/riot-os/riot.git"
+
+cd ${GITHUB_WORKSPACE} && git fetch ${RIOT_REPO} master && bash -c "${INPUT_CMD}"
