3a24208fc1a5cb6ab1a73cf871e5ec25422e88d8
Alexandre Abadie <alexandre.abadie@inria.fr>
2020-07-06 11:58:25 +0200

    entrypoint.sh: fix invalid syntax

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,6 @@
 
 set -e
 
-RIOT_REPO = "https://github.com/riot-os/riot.git"
+RIOT_REPO="https://github.com/riot-os/riot.git"
 
 cd ${GITHUB_WORKSPACE} && git fetch ${RIOT_REPO} master && bash -c "${INPUT_CMD}"
